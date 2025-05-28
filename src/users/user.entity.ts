3401913ee587fb25b22a39ddf307e029483ff65a
Vadim <andoskinvadim2022@gmail.com>
2025-05-28 14:18:16 +0300

    first commit

---
+++ src/users/user.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+
+
+@Entity()
+export class User {
+  @PrimaryColumn()
+  id: string;
+
+
+  @Column()
+  firstname: string;
+
+
+  @Column({ default: 0 })
+  points: number;
+}
